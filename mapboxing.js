8ca970eacd4b0f5ccc944585d795a9e4129c18c9
aljedaxi <aljedaxi@gmail.com>
2019-10-14 17:53:52 -0600

    the javascript SDK is useless

--- mapboxing.js
+++ mapboxing.js
@@ -1,10 +1,43 @@
 'use strict';
 
+const mbxUploads = require('@mapbox/mapbox-sdk/services/uploads');
+const uploadsClient = mbxUploads({accessToken: process.env.TOKEN});
+const AWS = require('aws-sdk');
+
+const ownerId = process.env.USERNAME;
+const TEST_DATA_PATH = 'test.geojson';
+
 console.log(process.env.USERNAME);
 console.log(process.env.TOKEN);
 
+const getCredentials = () => {
+  return uploadsClient
+    .createUploadCredentials()
+    .send()
+    .then(response => response.body);
+};
+
+const putFileOnS3 = (credentials) => {
+  const s3 = new AWS.S3({
+    accessKeyId: credentials.accessKeyId,
+    secretAccessKey: credentials.secretAccessKey,
+    sessionToken: credentials.sessionToken,
+    region: 'us-east-1'
+  });
+  return s3.putObject({
+    Bucket: credentials.bucket,
+    Key: credentials.key,
+    Body: fs.createReadStream(TEST_DATA_PATH)
+  }).promise();
+};
+
+getCredentials()
+	.then(putFileOnS3)
+	.catch(console.error);
+
+
 /*
-function upload_to_mapbox(geojson_doc, area_id) {
+Function upload_to_mapbox(geojson_doc, area_id) {
 	const MY_ACCESS_TOKEN = 'testest'; //TODO
 	const USERNAME = 'testest'; //TODO
 	const AWS = require('aws-sdk'); //TODO move to top
